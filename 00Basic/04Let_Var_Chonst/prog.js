01bf4e92d462da2da7ef5e251a7e9dca15d6e8ab
Mridu Mallick <mridumallick101@gmail.com>
2025-08-31 12:08:30 +0530

    Create prog.js

---
+++ 00Basic/04Let_Var_Chonst/prog.js
@@ -0,0 +1,21 @@
+var x = 8.9;  // var simply reflect typical variable concept
+console.log(x);
+x = true;
+console.log(x);
+x = [2,6,3,8];
+console.log(x);
+
+let z = 'P';  // let is also similar to var, but they have some difference.
+console.log(z);
+z = "Happy";
+console.log(z);
+
+
+const y = "Hello";   // const means this a variable can't change
+console.log(y);
+// y = 8;
+
+// var to const or const to var is not possible
+// var r = 8;
+// console.log(r);
+// const r = 9;  // --> ERROR
