909ad059baf9c135973a56bfbfa870984f94e132
Mridu Mallick <mridumallick101@gmail.com>
2025-08-31 11:27:23 +0530

    Create 00prog.js

---
+++ 00Basic/01Variable/00prog.js
@@ -0,0 +1,13 @@
+// Variables type does't matter in java Script
+a = 10
+console.log(a);
+a = 3.14
+console.log(a);
+a = 'G'
+console.log(a);
+a = "Mridu"
+console.log(a);
+a = true
+console.log(a)
+
+console.log(typeof a);
