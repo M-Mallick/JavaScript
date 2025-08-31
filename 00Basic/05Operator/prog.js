13057d48ec551f615f620a85b157884451e8a302
Mridu Mallick <mridumallick101@gmail.com>
2025-08-31 12:10:52 +0530

    Create prog.js

---
+++ 00Basic/05Operator/prog.js
@@ -0,0 +1,10 @@
+a = 10;
+b = 6;
+console.log(a+b);  // Addition
+console.log(b-a);  // subtraction
+console.log(a*b);  // Multiplication
+console.log(a/b);  // Division
+console.log(a**b);  // power => pow(a,b);
+console.log(Math.floor(a/b));  // after division, to get integer value
+
+// a++ and ++a
