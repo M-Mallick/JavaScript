b6fb6845157a1792c4c4079e3ba3d7a0210aba0f
Mridu Mallick <mridumallick101@gmail.com>
2025-09-10 23:05:04 +0530

    Update temp.js

--- 02_String/temp.js
+++ 02_String/temp.js
@@ -1,1 +1,31 @@
-0
+{
+    // Trim Method --> str.trim();
+    let str = " Hello World   ";
+    console.log(str.trim());
+    console.log(str);
+}
+
+
+console.log("--next--");
+{
+    /* ToUpperCase and ToLowerCase Method
+        --> str.toUpperCase();
+        --> str.toLowerCase();
+    */
+    let str = "Random String";
+    console.log(str.toUpperCase());
+    console.log(str.toLowerCase());
+    console.log(str);
+}
+
+
+console.log("--next--");
+{
+    let str = "ILOVECODING";
+    console.log(str.indexOf("CO"));
+    console.log(str.indexOf("LOVE"));
+    console.log(str.indexOf("IN"));
+}
+
+console.log("--next--");
+console.log();
