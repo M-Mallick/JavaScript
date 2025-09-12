63a4ccf7bdf7728cb6a6eb27121d96d6b78a601f
Mridu Mallick <mridumallick101@gmail.com>
2025-09-12 23:18:49 +0530

    Update temp.js

--- 03_Array/Basic Method/temp.js
+++ 03_Array/Basic Method/temp.js
@@ -1,1 +1,12 @@
-....
+arr = [1,2,3,5];
+console.log(arr);
+console.log("element at 0 : ",arr[0]);
+arr.push(84);  // to add from the back side
+console.log(arr);
+arr.pop();     // to remove from the back side
+console.log(arr);
+
+arr.unshift(67);  // to add from the front side
+console.log(arr);
+arr.shift();      // to remove from the front side
+console.log(arr);
