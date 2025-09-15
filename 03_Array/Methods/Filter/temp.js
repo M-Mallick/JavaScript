d338edb43d914ecd0a88bd0da9565784ca9f834c
Mridu Mallick <mridumallick101@gmail.com>
2025-09-15 23:00:40 +0530

    Update temp.js

--- 03_Array/Methods/Filter/temp.js
+++ 03_Array/Methods/Filter/temp.js
@@ -1,1 +1,12 @@
-..
+const products = [
+    { id: 1, name: 'Product 1', price: 40 },
+    { id: 2, name: 'Product 2', price: 60 },
+    { id: 3, name: 'Product 3', price: 30 }
+  ];
+  const expensiveProducts = [];
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].price > 50) {
+      expensiveProducts.push(products[i]);
+    }
+  }
+  // expensiveProducts: [{ id: 2, name: 'Product 2', price: 60 }]
