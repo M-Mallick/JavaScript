8fac8a94f065060b8da5e5d0ebba9651f792c589
Mridu Mallick <mridumallick101@gmail.com>
2025-09-15 22:57:42 +0530

    Update temp.js

--- 03_Array/Methods/Filter/temp.js
+++ 03_Array/Methods/Filter/temp.js
@@ -1,1 +1,14 @@
-..
+// Without filter method
+const values = [10, null, 20, undefined, 30];
+const validValues1 = [];
+for(let i=0; i < values.length; i++) {
+    if(values[i] != null && values[i] != undefined) {
+        validValues1.push(values[i]);
+    }
+}
+
+console.log("First : "+validValues1);
+
+// with filter method
+validValues2 = values.filter( (v) => v != null && v != undefined);
+console.log("Second : "+validValues2);
