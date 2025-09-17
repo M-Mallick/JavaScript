f3a9e643a2f5a32d108b67d8bc6662a1e08d0a56
Mridu Mallick <mridumallick101@gmail.com>
2025-09-17 23:11:34 +0530

    Update temp.js

--- 03_Array/Methods/Map()/temp.js
+++ 03_Array/Methods/Map()/temp.js
@@ -1,1 +1,12 @@
-..
+/*
+    It is used to create a new array by applying a provided 
+    callback function to each element of an existing array. 
+    The original array remains unchanged.
+*/
+const numbers = [1, 2, 3, 4];
+
+const numberCube = numbers.map(function(num) {
+    return num ** 3;
+})
+
+console.log(numberCube);
