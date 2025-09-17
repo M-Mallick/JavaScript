8585d42b4c737265c929474022b4e25d0b7aa9d0
Mridu Mallick <mridumallick101@gmail.com>
2025-09-17 23:26:03 +0530

    Update temp.js

--- 04_Object/temp.js
+++ 04_Object/temp.js
@@ -1,1 +1,14 @@
-..
+const data = {
+	name : "Adrima",
+	age : 22,
+	height : undefined,
+	eatting : function (food) {
+		// body...
+		console.log(`I like ${food}`);
+	}
+}
+
+console.log(data['name']);
+console.log(data.age);
+console.log(data.height);
+data.eatting('Pizza');
