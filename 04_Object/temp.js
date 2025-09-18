9382ec6055a49e71fbc56679966216e0ff843aa5
Mridu Mallick <mridumallick101@gmail.com>
2025-09-18 23:26:52 +0530

    Update temp.js

--- 04_Object/temp.js
+++ 04_Object/temp.js
@@ -1,1 +1,16 @@
-..
+const objInfo = {
+  obj1: {
+    name: "Mridu Mallick",
+    course: "IT"
+  },
+  obj2: {
+    name: "Adrima Biswas",
+    course: "IT"
+  },
+  obj3: {
+    name: "Surjo Saha",
+    course: "ECE"
+  }
+};
+
+console.log(objInfo);
