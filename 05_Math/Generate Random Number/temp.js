27486cdf887647b8c87ab61cc38453208706881b
Mridu Mallick <mridumallick101@gmail.com>
2025-09-18 23:29:59 +0530

    Update temp.js

--- 05_Math/Generate Random Number/temp.js
+++ 05_Math/Generate Random Number/temp.js
@@ -1,1 +1,14 @@
-..
+// Function to generate OTP
+function generateOTP() {
+	let digits = '0123456789'; // 10
+	let OTP = '';
+	let len = digits.length
+	for (let i = 0; i < 4; i++) {
+		OTP += digits[Math.floor(Math.random() * len)]; // 0.536*10 = 5.36 --> floor = 5
+	}
+
+	return OTP;
+}
+
+console.log("OTP of 4 digits: ")
+console.log(generateOTP());
