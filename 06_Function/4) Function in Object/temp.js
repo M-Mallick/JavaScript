504224e8cd62846e3daddfbeee4ad0ac450a0cac
Mridu Mallick <mridumallick101@gmail.com>
2025-09-21 23:19:22 +0530

    Update temp.js

--- 06_Function/4) Function in Object/temp.js
+++ 06_Function/4) Function in Object/temp.js
@@ -1,1 +1,12 @@
-..
+const calculation = {
+  add : function (a, b) {
+    return a + b;
+  },
+  sub : function (a, b) {
+    return a - b;
+  },
+  mul : function (a, b) {
+    return a * b;
+  }
+}
+console.log(calculation.sub(2.9, 7.1));
