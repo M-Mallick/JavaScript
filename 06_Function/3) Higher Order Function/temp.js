6dbc91b25268e0532ff33c6b3a677846554bdd65
Mridu Mallick <mridumallick101@gmail.com>
2025-09-21 23:09:32 +0530

    Update temp.js

--- 06_Function/3) Higher Order Function/temp.js
+++ 06_Function/3) Higher Order Function/temp.js
@@ -1,1 +1,17 @@
-..
+function oddEvenTest(request) {
+    if(request == "odd") {
+        return function (n) {
+            console.log(!(n%2 == 0));
+        }
+    }
+    else if(request == "even") {
+        return function(n) {
+            console.log(n%2 == 0);
+        }
+    }
+    else {
+        console.log("Wrong Request");
+    }
+}
+let a = oddEvenTest("odd");
+console.log(a(20));
