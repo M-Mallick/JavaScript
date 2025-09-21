d96063de63c030f712364aed1522eb3225a4be1b
Mridu Mallick <mridumallick101@gmail.com>
2025-09-21 23:07:17 +0530

    Update temp.js

--- 06_Function/3) Higher Order Function/temp.js
+++ 06_Function/3) Higher Order Function/temp.js
@@ -1,1 +1,14 @@
-..
+function multipleGreed(fun, count) {  //  Higher Order Function
+  for (let i = 1; i <= count; i++) {
+    fun(); // Call the function inside the loop
+  }
+}
+
+let greed = function() {
+  console.log("Hello, Mridu");
+};
+
+// Pass the function reference instead of calling it immediately
+multipleGreed(greed, 10);
+
+multipleGreed(function(){console.log("OK!");}, 10); 
