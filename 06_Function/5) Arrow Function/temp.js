9a4f1a1a7bd055d82bfd494dfc458aee6c9f9c15
Mridu Mallick <mridumallick101@gmail.com>
2025-09-22 23:26:36 +0530

    Update temp.js

--- 06_Function/5) Arrow Function/temp.js
+++ 06_Function/5) Arrow Function/temp.js
@@ -1,1 +1,17 @@
-..
+/*
+  > An arrow function is a shorter syntax for writing functions in JavaScript
+  > They don’t have their own this. They inherit this from the surrounding context.
+*/
+// Regular Function
+let a = function () {
+  console.log("Hello R.F.");
+}
+a();
+
+// Arrow Function
+let a1 = () => {
+  console.log("Hello A.F.");
+}
+a1();
+
+
