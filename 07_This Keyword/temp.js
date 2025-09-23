f6c7e0d7886673a33748af02e24c5501280c34d1
Mridu Mallick <mridumallick101@gmail.com>
2025-09-23 23:24:18 +0530

    Update temp.js

--- 07_This Keyword/temp.js
+++ 07_This Keyword/temp.js
@@ -1,1 +1,18 @@
-..
+const student = {
+  name : "Mridu Mallick",
+  age : 22,
+  eng : 80,
+  math : 90,
+  phy : 95,
+  getAvg() {
+    let avg = (this.eng + this.math + this.phy) / 3;
+    console.log(`${this.name} got avg marks = ${avg}`);
+  }
+}
+
+student.getAvg();
+
+function test() {
+  console.log(this);  // In a function, the global object is the default binding for this.
+}                     // In a browser window the global object is [object Window]
+test();
