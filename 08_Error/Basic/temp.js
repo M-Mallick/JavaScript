77a4c4590e5185cf13feef5913714e0e9d09dbd9
Mridu Mallick <mridumallick101@gmail.com>
2025-09-23 23:30:17 +0530

    Update temp.js

--- 08_Error/Basic/temp.js
+++ 08_Error/Basic/temp.js
@@ -1,1 +1,19 @@
-..
+/*
+    try {
+         Try Block to check for errors.
+    }
+    catch(err) {
+          Catch Block to display errors.
+    }
+*/
+console.log("Hello 1");
+console.log("Hello 2");
+// console.log(a); Error
+try {
+  console.log(a);
+}
+catch(err) {
+  console.log("Somthing Wrong");
+}
+console.log("Welcome 1");
+console.log("Welcome 2");
