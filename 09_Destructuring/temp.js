4c035ece58c807bc1b1e2338bece9f3f3744355f
Mridu Mallick <mridumallick101@gmail.com>
2025-09-24 23:59:39 +0530

    Update temp.js

--- 09_Destructuring/temp.js
+++ 09_Destructuring/temp.js
@@ -1,1 +1,13 @@
-..
+// Imagine this is your "req.body"
+const req = {
+  body: {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald"
+  }
+};
+
+// Destructure
+const { title, author } = req.body;
+
+console.log(title);   // The Great Gatsby
+console.log(author);  // F. Scott Fitzgerald
