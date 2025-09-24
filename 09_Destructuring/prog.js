70e26e14525b7aa935a828874a5de8377ebe3771
Mridu Mallick <mridumallick101@gmail.com>
2025-09-24 23:57:01 +0530

    Update prog.js

--- 09_Destructuring/prog.js
+++ 09_Destructuring/prog.js
@@ -1,1 +1,15 @@
-/..
+color = ['Red', 'Green', 'Blue'];
+const [f] = color // index = 0
+console.log(f);
+
+const [,g] = color // index = 1
+console.log(g);
+
+const [,,b] = color // index = 2
+console.log(b);
+
+const [,,,u] = color
+console.log(u);
+
+const [firstColor, secondColor, thirdColor] = color;
+console.log(`${firstColor} , ${secondColor}, ${thirdColor}`);
