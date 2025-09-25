540de05a1970ecb6b7713375c7bd524e725e19cb
Mridu Mallick <mridumallick101@gmail.com>
2025-09-25 23:31:19 +0530

    Update temp.js

--- 10_TimeOut & Interval Methods/temp.js
+++ 10_TimeOut & Interval Methods/temp.js
@@ -1,1 +1,11 @@
-..
+// Syntax : setTimeOut(function, timeOut);
+
+console.log("Let's start --> ");
+
+setTimeout(function(){console.log("1st Iteration By MM")}, 3000); // Regular Function
+setTimeout(function(){console.log("2nd Iteration By MM")}, 5000); // Regular Function
+setTimeout(() => {console.log("3rd Iteration By MM")}, 7000); // Arrow Function
+setTimeout(() => {console.log("4th and last Iteration By MM")}, 9000); // Arrow Function
+
+console.log("last ...");
+
