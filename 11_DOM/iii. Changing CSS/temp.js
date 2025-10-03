5ef6b5be7f30502988d85487a78af46f2fc17d16
Mridu Mallick <mridumallick101@gmail.com>
2025-10-03 23:48:32 +0530

    Update temp.js

--- 11_DOM/iii. Changing CSS/temp.js
+++ 11_DOM/iii. Changing CSS/temp.js
@@ -1,1 +1,12 @@
-..
+// let a = document.querySelectorAll(".box1 li");
+
+let b = document.querySelectorAll(".box2 li");
+
+// By using forEach method.
+// b.forEach(function(item) {
+//     item.style.color = "green";
+// });
+ // By using for loop.
+for(let i=0; i < b.length; i++) {
+    b[i].style.color = "blue";
+}
